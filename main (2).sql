bdc1b48298f40e033477327dd4b933312972200d
jishnuck663 <68513265+jishnuck663@users.noreply.github.com>
2020-11-27 15:20:45 +0530

    Add files via upload

---
+++ main (2).sql
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                        Online SQLite Query Runner.
+                Code, Compile, Run and Debug SQLite query online.
+Write your query in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+
+/* Enter your sql queries here */
+CREATE TABLE DEPARTMENT (
+CODE VARCHAR(20),
+TITLE CHAR(15),
+DEPT_NAME CHAR(15) UNIQUE,
+DEPT_ID INT,
+SALARY INT CHECK(SALARY>2000),
+PRIMARY KEY(CODE)
+);
+INSERT INTO DEPARTMENT VALUE
+('B01','STAFF','CS',01,22000);
+
+CREATE TABLE INSTRUCTOR (
+NAME CHAR(20) NOT NULL,
+CODE VARCHAR(20),
+ID INT DEFAULT 0
+);
+INSERT INTO INSTRUCTOR VALUES
+('SIRI','E22',30);
